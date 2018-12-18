f7f7bc982d2a94d0c61f3804ffd9936898605591
liangdeqing <deqing_ios@163.com>
2018-12-18 14:44:59 +0800

    init

---
+++ DQObjectCMaster/DQObjectCMaster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DQObjectCMaster
+//
+//  Created by wondertek on 2018/12/18.
+//  Copyright © 2018年 deqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DQObjectCMaster/DQObjectCMaster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DQObjectCMaster
+//
+//  Created by wondertek on 2018/12/18.
+//  Copyright © 2018年 deqing. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
